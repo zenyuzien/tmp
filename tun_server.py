3aa7dca758082d749db80e7586bd5ff933aee316
RUSHIKESH MURAHARISETTY <97795983+zenyuzien@users.noreply.github.com>
2026-05-08 02:13:02 -0500

    Update tun_server.py

--- tun_server.py
+++ tun_server.py
@@ -1,15 +1,29 @@
-
 #!/usr/bin/python3
+import fcntl, struct, os, socket
 from scapy.all import *
 
-IP_A = "0.0.0.0"
+TUNSETIFF = 0x400454ca
+IFF_TUN   = 0x0001
+IFF_NO_PI = 0x1000
+
 PORT = 9090
 
+# Create TUN
+tun = os.open("/dev/net/tun", os.O_RDWR)
+ifr = struct.pack('16sH', b'tun%d', IFF_TUN | IFF_NO_PI)
+ifname_bytes = fcntl.ioctl(tun, TUNSETIFF, ifr)
+ifname = ifname_bytes.decode('UTF-8')[:16].strip("\x00")
+print("Interface Name: {}".format(ifname))
+
+os.system("ip addr add 192.168.53.98/24 dev {}".format(ifname))
+os.system("ip link set dev {} up".format(ifname))
+os.system("sysctl -w net.ipv4.ip_forward=1")
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((IP_A, PORT))
+sock.bind(("0.0.0.0", PORT))
 
 while True:
     data, (ip, port) = sock.recvfrom(2048)
-    print("{}:{} --> {}:{}".format(ip, port, IP_A, PORT))
     pkt = IP(data)
-    print("  Inside: {} --> {}".format(pkt.src, pkt.dst))
+    print("From tunnel: {} --> {}".format(pkt.src, pkt.dst))
+    os.write(tun, data)
